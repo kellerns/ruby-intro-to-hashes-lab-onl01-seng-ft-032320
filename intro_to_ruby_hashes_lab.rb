f712792cf48ebb9d4531506db6482268058c9353
Nicholas Keller <nic.keller328@gmail.com>
2020-03-31 14:33:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,24 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads:{pieces: 4, rent_in_dollars: {}}}
+	monopoly = {
+	  railroads:
+	  {
+	    pieces: 4,
+	    rent_in_dollars: 
+	    {
+	    },
+	    names: 
+	    {
+	      reading_railroad: {},
+	      pennsylvania_railroad: {},
+	      b_and_o_railroad: {},
+	      shortline: {}
+	      
+	    }
+	  }
+	  
+	}
 
 end
 
